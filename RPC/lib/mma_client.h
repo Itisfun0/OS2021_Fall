4a27186ae93dc51a8a3c7c9a258a4424c5f1aba2
Itisfun0 <1549861585@qq.com>
2022-01-04 08:09:11 +0800

    update

--- RPC/lib/mma_client.h
+++ RPC/lib/mma_client.h
@@ -12,14 +12,24 @@
 #include "mma.grpc.pb.h"
 #endif
 
+using mma::Greeter;
+#include "array_list.h"
+using grpc::Channel;
 
 namespace proj4 {
-
 class ArrayList;
 
 class MmaClient {
+public:
+    MmaClient(std::shared_ptr<Channel>);
+    ArrayList* Allocate(size_t);
+    void Free(ArrayList*);
+    int Read (int, int, int);
+    void Write (int, int, int, int);
+    ~MmaClient(){}
+private:
+  std::unique_ptr<Greeter::Stub> stub_;
+  int next_array_id;
 };
 
 } //namespace proj4
-
-#endif
--- RPC/lib/mma_server.h
+++ RPC/lib/mma_server.h
@@ -16,12 +16,34 @@
 #include "mma.grpc.pb.h"
 #endif
 
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using mma::Greeter;
+using mma::AllocateRequest;
+using mma::AllocateReply;
+using mma::FreeRequest;
+using mma::ReadRequest;
+using mma::ReadReply;
+using mma::WriteRequest;
+using mma::Empty;
+
 #include "memory_manager.h"
 
 
 // Logic and data behind the server's behavior.
 
 namespace proj4 {
+
+class GreeterServiceImpl final : public Greeter::Service {
+    Status Allocate(ServerContext * context, const AllocateRequest* request, AllocateReply* reply);
+    Status Free(ServerContext * context, const FreeRequest* request, Empty* reply);
+    Status Read(ServerContext * context, const ReadRequest* request, ReadReply* reply);
+    Status Write(ServerContext * context, const WriteRequest* request, Empty* reply);
+};
 
 // setup a server with UnLimited virtual memory space
 void RunServerUL(size_t phy_page_num);
